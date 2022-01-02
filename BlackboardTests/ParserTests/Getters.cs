78ff19a4e9f723e2656897186599b3990859c87f
Grant Nelson <grantnelson1@gmail.com>
2022-01-02 02:01:49 -0700

    Update Getters.cs

--- BlackboardTests/ParserTests/Getters.cs
+++ BlackboardTests/ParserTests/Getters.cs
@@ -8,13 +8,29 @@ namespace BlackboardTests.ParserTests {
     public class Getters {
 
         [TestMethod]
-        public void TestBasicParses_TypedInput() {
+        public void TestBasicParses_TypedGetter() {
             Slate slate = new();
             Result result = slate.Read(
                 "get int A = 3;",
                 "get bool B = true;",
                 "get double C = 4;",
                 "get string D = 'Boom stick';").
+                Perform();
+
+            result.CheckValue(3, "A");
+            result.CheckValue(true, "B");
+            result.CheckValue(4.0, "C");
+            result.CheckValue("Boom stick", "D");
+        }
+        
+        [TestMethod]
+        public void TestBasicParses_VarGetter() {
+            Slate slate = new();
+            Result result = slate.Read(
+                "get A = 3;",
+                "get B = true;",
+                "get C = 4.0;",
+                "get D = 'Boom stick';").
                 Perform();
 
             result.CheckValue(3, "A");

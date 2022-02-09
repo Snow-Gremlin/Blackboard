2db8cc336b0ff80a7dd582b86acb09c6cb9e9430
Grant Nelson <grantnelson1@gmail.com>
2022-02-09 02:32:30 -0700

    Update FixedParents.cs

--- Blackboard/Core/Nodes/Collections/FixedParents.cs
+++ Blackboard/Core/Nodes/Collections/FixedParents.cs
@@ -5,125 +5,125 @@ using System.Linq;
 using S = System;
 
 namespace Blackboard.Core.Nodes.Collections {
-    public class FixedParents: IParentCollection {
-        private interface ISingleParam {
-            public S.Type Types { get; }
+    internal class FixedParents: IParentCollection {
+
+        private interface ISingleParent {
+            public S.Type Type { get; }
             public IParent Node { get; set; }
         }
 
-        private class SingleParam<T>: ISingleParam
+        private class SingleParent<T>: ISingleParent
                 where T : class, IParent {
 
             private readonly S.Func<T> getParent;
 
             private readonly S.Action<T> setParent;
 
-            public SingleParam(S.Func<T> getParent, S.Action<T> setParent) {
+            public SingleParent(S.Func<T> getParent, S.Action<T> setParent) {
                 this.getParent = getParent;
                 this.setParent = setParent;
             }
 
-            public S.Type Types => typeof(T);
+            public S.Type Type => typeof(T);
 
             public IParent Node {
                 get => this.getParent();
                 set => this.setParent(value as T);
             }
-
         }
 
-        private List<ISingleParam> parameters;
+        private List<ISingleParent> parents;
 
         public FixedParents(IChild child) {
             this.Child = child;
-            this.parameters = new List<ISingleParam>();
+            this.parents = new List<ISingleParent>();
+        }
+
+        internal FixedParents With<T>(S.Func<T> getParent, S.Action<T> setParent)
+            where T : class, IParent {
+            this.parents.Add(new SingleParent<T>(getParent, setParent));
+            return this;
         }
 
         public readonly IChild Child;
 
-        public IEnumerable<S.Type> Types => this.parameters.Select((p) => p.Types);
+        /// <summary>The set of type that each parent could have in this collection.</summary>
+        /// <remarks>For N-ary nodes this can be very long so use this with zip or limit the enumerations.</remarks>
+        public IEnumerable<S.Type> Types => this.parents.Select((p) => p.Type);
 
-        public IEnumerable<IParent> Nodes => this.parameters.Select((p) => p.Node).NotNull();
+        /// <summary>The set of parent nodes to this node.</summary>
+        /// <remarks>
+        /// This should not contain null parents, but it might contain repeat parents.
+        /// For example, if a number is the sum of itself (x + x), then the Sum node will return the 'x' parent twice.
+        /// </remarks>
+        public IEnumerable<IParent> Nodes => this.parents.Select((p) => p.Node).NotNull();
 
-        internal FixedParents With<T>(S.Func<T> getParent, S.Action<T> setParent)
-            where T : class, IParent {
-            this.parameters.Add(new SingleParam<T>(getParent, setParent));
-            return this;
-        }
-
-        public bool ReplaceParent(IParent oldParent, IParent newParent) => throw new S.NotImplementedException();
-
-        public bool SetAllParents(List<IParent> newParents) => throw new S.NotImplementedException();
-
-        /* 
         /// <summary>This replaces all instances of the given old parent with the given new parent.</summary>
+        /// <remarks>
+        /// The new parent must be able to take the place of the old parent,
+        /// otherwise this will throw an exception when attempting the replacement of the old parent.
+        /// </remarks>
         /// <param name="oldParent">The old parent to find all instances with.</param>
         /// <param name="newParent">The new parent to replace each instance with.</param>
         /// <returns>True if any parent was replaced, false if that old parent wasn't found.</returns>
-        public bool ReplaceParent(IParent oldParent, IParent newParent) =>
-            IChild.ReplaceParent(this, ref this.source1, oldParent, newParent) |
-            IChild.ReplaceParent(this, ref this.source2, oldParent, newParent);
+        public bool ReplaceParent(IParent oldParent, IParent newParent) {
+            if (!ReferenceEquals(oldParent, newParent)) return false;
+
+            bool changed = false;
+            for (int i = 0; i < this.parents.Count; ++i) {
+                ISingleParent parent = this.parents[i];
+                if (!ReferenceEquals(parent.Node, oldParent)) continue;
+
+                IParent node = parent.Node;
+                if (newParent is not null && !newParent.GetType().IsAssignableTo(parent.Type))
+                    throw new Exception("Unable to replace old parent with new parent.").
+                        With("child", this.Child).
+                        With("node", node).
+                        With("index", i).
+                        With("old Parent", oldParent).
+                        With("new Parent", newParent);
+
+                bool removed = node?.RemoveChildren(this.Child) ?? false;
+                parent.Node = newParent;
+                if (removed) newParent?.AddChildren(this.Child);
+                changed = true;
+            }
+            return changed;
+        }
 
         /// <summary>This will attempt to set all the parents in a node.</summary>
         /// <remarks>This will throw an exception if there isn't the correct count or types.</remarks>
         /// <param name="newParents">The parents to set.</param>
         /// <returns>True if any parents changed, false if they were all the same.</returns>
         public bool SetAllParents(List<IParent> newParents) {
-            IChild.CheckParentsBeingSet(newParents, false, typeof(IValueParent<T1>), typeof(IValueParent<T2>));
-            return IChild.SetParent(this, ref this.source1, newParents[0] as IValueParent<T1>) |
-                   IChild.SetParent(this, ref this.source2, newParents[1] as IValueParent<T2>);
-        }
-        */
+            int count = newParents.Count;
+            if (this.parents.Count != count)
+                throw new Exception("Incorrect number of parents in the list of parents to set to a node.").
+                    With("child", this.Child).
+                    With("expected count", count).
+                    With("given count", this.parents.Count);
 
-        /*
-        /// <summary>This is a helper method for replacing a parent in the given node.</summary>
-        /// <typeparam name="T">The node type for the parent.</typeparam>
-        /// <param name="child">The child to replace the parent for.</param>
-        /// <param name="node">The parent node variable being replaced.</param>
-        /// <param name="oldParent">The old parent to check for.</param>
-        /// <param name="newParent">The new parent being set, or null</param>
-        /// <returns>True if replaced, false if not.</returns>
-        static protected bool ReplaceParent<T>(IChild child, ref T node, IParent oldParent, IParent newParent)
-            where T : class, IParent {
-            if (!ReferenceEquals(node, oldParent)) return false;
-            if (newParent is not null and not T)
-                throw new Exception("Unable to replace old parent with new parent.").
-                    With("child", child).
-                    With("node", node).
-                    With("old Parent", oldParent).
-                    With("new Parent", newParent);
-            bool removed = node?.RemoveChildren(child) ?? false;
-            node = newParent as T;
-            if (removed) node?.AddChildren(child);
-            return true;
-        }
-
-        /// <summary>This checks if the set of parents about to be set to this node is valid.</summary>
-        /// <param name="parents">The list of parents to check before being set.</param>
-        /// <param name="variableLength">
-        /// True if any number of parents may be set, false to check the count.
-        /// If true then there must be at least one type in the expected types.
-        /// </param>
-        /// <param name="expectedTypes">The expected types for the parents.</param>
-        static internal void CheckParentsBeingSet(List<IParent> parents, bool variableLength, params S.Type[] expectedTypes) {
-            if (!variableLength) {
-                int count = expectedTypes.Length;
-                if (parents.Count != count)
-                    throw new Exception("Incorrect number of parents in the list of parents to set to a node.").
-                        With("Expected Count", count).
-                        With("Given Count", parents.Count);
-            }
-
-            int index = 0;
-            foreach ((IParent parent, S.Type expectedType) in parents.Zip(expectedTypes)) {
-                if (parent.GetType().IsAssignableTo(expectedType))
+            for (int i = 0; i < count; ++i) {
+                IParent newParent = newParents[i];
+                ISingleParent parent = this.parents[i];
+                if (newParent.GetType().IsAssignableTo(parent.Type))
                     throw new Exception("Incorrect type of a parent in the list of parents to set to a node.").
-                        With("Index", index).
-                        With("Expected Type", expectedType).
-                        With("Gotten Type", parent.GetType());
-                ++index;
+                        With("child", this.Child).
+                        With("index", i).
+                        With("expected Type", parent.Type).
+                        With("gotten Type", newParent.GetType());
             }
+
+            bool changed = false;
+            for (int i = 0; i < count; ++i) {
+                IParent node = this.parents[i].Node;
+                bool removed = node?.RemoveChildren(this.Child) ?? false;
+                IParent newParent = newParents[i];
+                this.parents[i].Node = newParent;
+                if (removed) newParent?.AddChildren(this.Child);
+                changed = true;
+            }
+            return changed;
         }
-        */
     }
 }

01104d9493945f31e17a088c0f879dd8bfb92a3b
Grant Nelson <grantnelson1@gmail.com>
2022-03-24 00:36:34 -0600

    Leaving the diagram tool as is for now

--- BlackboardTools/Commands/Diagram.cs
+++ BlackboardTools/Commands/Diagram.cs
@@ -11,10 +11,11 @@ namespace BlackboardTools.Commands {
 
         public string ShortDescription => "Creates a UML diagram for the classes in a given namespace.";
 
-        public string FullDescription => "Creates a UML (mermaid) diagram for classes in a given namespace. "+
+        public string FullDescription => "Creates a UML diagram for classes in a given namespace. "+
             "Example \"diagram Blackboard.Core.Nodes\". The resulting diagram commands is printed to the console. "+
             "The internal namespaces to the one which is given are also added to the diagram. "+
-            "This is useful to update the diagrams in the README.md files.";
+            "This is useful to update the diagrams in the README.md files. "+
+            "NOTE: This doesn't work correctly yet.";
 
         private class Node {
             readonly public S.Type Type;
@@ -29,15 +30,12 @@ namespace BlackboardTools.Commands {
             args.RequiresExactly(1);
             string nspace = args.Arguments[0];
             Dictionary<string, Node> allTypes = getAllTypes(nspace);
-            Dictionary<string, List<Node>> grouped = groupTypes(allTypes);
 
             StringBuilder buf = new();
-            buf.AppendLine("flowchart TB");
-            int nspaceIndex = 0;
-            foreach ((string group, List<Node> nodes) in grouped) {
-                addGroup(buf, nspaceIndex, group, nodes, allTypes);
-                nspaceIndex++;
-            }
+            foreach (Node node in allTypes.Values)
+                addClassDef(buf, node);
+            foreach (Node node in allTypes.Values)
+                addInheritance(buf, node, allTypes);
             S.Console.WriteLine(buf);
         }
 
@@ -45,22 +43,12 @@ namespace BlackboardTools.Commands {
             type.Namespace+"::"+type.Name;
 
         static private Dictionary<string, Node> getAllTypes(string nspace) =>
-            S.AppDomain.CurrentDomain.GetAssemblies().SelectMany(t => t.GetTypes()).
-            NotNull().Where(t => t.IsPublic).
+            S.AppDomain.CurrentDomain.GetAssemblies().SelectMany(t => t.GetTypes()).NotNull().
+            Where(t => t.IsPublic).
             Where(t => t.IsClass || t.IsInterface || t.IsAbstract).
             Where(t => t.Namespace is not null && (t.Namespace == nspace || t.Namespace.StartsWith(nspace+"."))).
-            WithIndex().Select(pair => new Node(pair.item, pair.index)).ToDictionary(n => getTypeKey(n.Type));
-
-        static private Dictionary<string, List<Node>> groupTypes(Dictionary<string, Node> allTypes) =>
-            allTypes.Values.GroupBy(n => n.Type.Namespace).
-            ToDictionary(g => g.Key, g => g.ToList());
-
-        static private void addGroup(StringBuilder buf, int nspaceIndex, string group, List<Node> nodes, Dictionary<string, Node> allTypes) {
-            buf.AppendLine("subgraph NSpace"+nspaceIndex+" ["+group+"]");
-            foreach (Node node in nodes)
-                addNode(buf, node, allTypes);
-            buf.AppendLine("end");
-        }
+            WithIndex().Select(pair => new Node(pair.item, pair.index)).
+            ToDictionary(n => getTypeKey(n.Type));
 
         static private string getSimpleTypeName(S.Type type) {
             string name = type.Name;
@@ -78,7 +66,7 @@ namespace BlackboardTools.Commands {
                 S.Type[] genTypes = type.GetGenericArguments();
                 foreach (S.Type genType in genTypes) {
                     string gen = getSimpleTypeName(genType);
-                    // TODO: Constraints?
+                    // Need to add Constraints?
                     gens.Add(gen);
                 }
 
@@ -87,15 +75,20 @@ namespace BlackboardTools.Commands {
             return name;
         }
 
-        static private void addNode(StringBuilder buf, Node node, Dictionary<string, Node> allTypes) {
+        static private void addClassDef(StringBuilder buf, Node node) {
             string name = getTypeName(node.Type);
-            if (node.Type.IsInterface)
-                buf.AppendLine("  "+node.Label+"[/"+name+"/]");
-            else buf.AppendLine("  "+node.Label+"["+name+"]");
+            string ctype =
+                node.Type.IsInterface ? "interface" :
+                node.Type.IsAbstract  ? "abstract"  :
+                "class";
 
+            buf.AppendLine(ctype+" \""+name+"\" as "+node.Label);
+        }
+
+        static private void addInheritance(StringBuilder buf, Node node, Dictionary<string, Node> allTypes) {
             foreach (S.Type inter in node.Type.GetInterfaces()) {
                 if (allTypes.TryGetValue(getTypeKey(inter), out Node other))
-                    buf.AppendLine("    "+other.Label+"-->"+node.Label);
+                    buf.AppendLine(node.Label+" --> "+other.Label);
             }
         }
     }

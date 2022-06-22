37196a94ce545a2d90c0092139663228be4e8602
Grant Nelson <grantnelson1@gmail.com>
2022-06-22 02:29:20 -0600

    Update Functions.cs

--- BlackboardTests/ParserTests/Functions.cs
+++ BlackboardTests/ParserTests/Functions.cs
@@ -1,7 +1,9 @@
 ﻿using Blackboard.Core;
 using Blackboard.Core.Extensions;
+using Blackboard.Core.Inspect;
 using BlackboardTests.Tools;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
+using S = System;
 
 namespace BlackboardTests.ParserTests {
 
@@ -14,6 +16,45 @@ namespace BlackboardTests.ParserTests {
                 TestTools.FuncDefTags(new Slate().Global).WhereNot(tag => tag.StartsWith(Slate.OperatorNamespace)),
                 TestTools.TestTags(this.GetType()),
                 "Tests do not match the existing function");
+
+        [TestMethod]
+        [TestTag("abs:Abs<Double>")]
+        public void TestFunctions_abs_Abs_Double() {
+            Slate slate = new Slate().Perform("in double A; B := abs(A);");
+            slate.CheckNodeString(Stringifier.Basic(), "B", "B: Abs<double>");
+            slate.Perform("A =   0.0; ").CheckValue(0.0,   "B");
+            slate.Perform("A =   1.0; ").CheckValue(1.0,   "B");
+            slate.Perform("A =  -1.0; ").CheckValue(1.0,   "B");
+            slate.Perform("A =  42.03;").CheckValue(42.03, "B");
+            slate.Perform("A = -42.03;").CheckValue(42.03, "B");
+        }
+
+        [TestMethod]
+        [TestTag("abs:Abs<Int>")]
+        public void TestFunctions_abs_Abs_Int() {
+            Slate slate = new Slate().Perform("in int A; B := abs(A);");
+            slate.CheckNodeString(Stringifier.Basic(), "B", "B: Abs<int>");
+            slate.Perform("A =   0;").CheckValue( 0, "B");
+            slate.Perform("A =   1;").CheckValue( 1, "B");
+            slate.Perform("A =  -1;").CheckValue( 1, "B");
+            slate.Perform("A =  42;").CheckValue(42, "B");
+            slate.Perform("A = -42;").CheckValue(42, "B");
+        }
+
+        [TestMethod]
+        [TestTag("acos:UnaryFuncs<Double, Double>")]
+        public void TestFunctions_acos_UnaryFuncs_Double_Double() {
+            Slate slate = new Slate().Perform("in double A; B := acos(A);");
+            slate.CheckNodeString(Stringifier.Basic(), "B", "B: Acos<double>");
+            slate.Perform("A =  0.0;").CheckValue(S.Math.PI*0.5, "B");
+            slate.Perform("A =  1.0;").CheckValue(0.0,           "B");
+            slate.Perform("A = -1.0;").CheckValue(S.Math.PI,     "B");
+            slate.Perform("A =  1.1;").CheckValue(double.NaN,    "B");
+        }
+
+
+
+
 
 
     }

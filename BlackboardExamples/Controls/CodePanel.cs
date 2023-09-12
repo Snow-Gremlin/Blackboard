e38b887ff167d15c6e12f9a36c8019f80bd25c05
Grant Nelson <grantnelson1@gmail.com>
2023-09-11 21:50:18 -0600

    Update CodePanel.cs

--- BlackboardExamples/Controls/CodePanel.cs
+++ BlackboardExamples/Controls/CodePanel.cs
@@ -1,25 +1,57 @@
-﻿using System;
-using System.Text;
+﻿using System.Text;
 
 namespace BlackboardExamples.Controls;
 
 // TODO: Comment
-sealed public class CodePanel : TextBox {
-     private static readonly string tabSpaces = "   ";
-     private static readonly int tabCount = tabSpaces.Length;
+sealed public class CodePanel : Panel {
+    private const int tabCount = 3;
+    private static readonly string tabSpaces = new(' ', tabCount);
     
+    private int lineIndex;
+    private RichTextBox editBox;
+
     public CodePanel() {
-        this.Dock       = DockStyle.Fill;
-        this.Font       = new Font(FontFamily.GenericMonospace, 9.0f);
-        this.ScrollBars = ScrollBars.Both;
-        this.Multiline  = true;
-        this.WordWrap   = false;
-        this.AcceptsTab = true;
+        this.lineIndex = 0;
+
+        this.SuspendLayout();
+        this.editBox             = new();
+        this.editBox.Anchor      = AnchorStyles.Top | AnchorStyles.Bottom | AnchorStyles.Left | AnchorStyles.Right;
+        this.editBox.Font        = new Font(FontFamily.GenericMonospace, 9.0f);
+        this.editBox.Location    = new Point(6, 0);
+        this.editBox.Size        = new Size(184, 200);
+        this.editBox.Multiline   = true;
+        this.editBox.WordWrap    = false;
+        this.editBox.AcceptsTab  = true;
+        this.editBox.BorderStyle = BorderStyle.None;
+
+        this.editBox.KeyDown          += this.onKeyDown;
+        this.editBox.SelectionChanged += this.onSelectionChanged;
+        this.editBox.VScroll          += this.onVScroll;
+        this.editBox.TextChanged      += this.onTextChanged;
+
+        this.Controls.Add(this.editBox);
+        this.BorderStyle = BorderStyle.Fixed3D;
+        this.Font        = new Font(FontFamily.GenericMonospace, 9.0f);
+        this.Size        = new Size(200, 200);
+        this.SetStyle(ControlStyles.UserPaint, true);
+        this.SetStyle(ControlStyles.AllPaintingInWmPaint, true);
+        this.SetStyle(ControlStyles.OptimizedDoubleBuffer, true);
+        this.SetStyle(ControlStyles.ResizeRedraw, true);
+        this.ResumeLayout(false);
     }
 
-    protected override void OnKeyDown(KeyEventArgs e) {
+    public string[] Lines {
+        get => this.editBox.Lines;
+        set => this.editBox.Lines = value;
+    }
+
+    public void Select(int start, int length) => this.editBox.Select(start, length);
+
+    public void ScrollToCaret() => this.editBox.ScrollToCaret();
+
+    private void onKeyDown(object? sender, KeyEventArgs e) {
         if (e.KeyCode == Keys.Tab) {
-            if (e.Modifiers == Keys.Shift) 
+            if (e.Modifiers == Keys.Shift)
                 this.handleBackwardTab();
             else this.handleForwardTab();
             e.Handled = true;
@@ -42,15 +74,15 @@ sealed public class CodePanel : TextBox {
     }
 
     private void handleBackwardTab() {
-        int length = this.SelectionLength;
+        int length = this.editBox.SelectionLength;
         if (length <= 0) {
             // Backwards tab with no selection (no op)
             return;
         }
-        
+
         // Backwards tab with selection
         StringBuilder buf = new(this.Text);
-        int start = this.SelectionStart;
+        int start = this.editBox.SelectionStart;
         int index = start + length;
         int count;
         do {
@@ -64,19 +96,19 @@ sealed public class CodePanel : TextBox {
         start -= count;
         length += count;
         this.Text = buf.ToString();
-        this.Select(start, length);
+        this.editBox.Select(start, length);
     }
 
     private void handleForwardTab() {
         StringBuilder buf = new(this.Text);
-        int start  = this.SelectionStart;
-        int length = this.SelectionLength;
-        
+        int start  = this.editBox.SelectionStart;
+        int length = this.editBox.SelectionLength;
+
         if (length <= 0) {
             // Forward tab with no selection
             buf.Insert(start, tabSpaces);
             start += tabCount;
-        }  else {
+        } else {
             // Forward tab with selection
             int index = start + length;
             do {
@@ -89,6 +121,58 @@ sealed public class CodePanel : TextBox {
         }
 
         this.Text = buf.ToString();
-        this.Select(start, length);
+        this.editBox.Select(start, length);
     }
+
+    private void onSelectionChanged(object? sender, EventArgs args) {
+        this.findLine();
+        this.Invalidate();
+    }
+    
+    private void onVScroll(object? sender, EventArgs e) {
+        this.findLine();
+        this.Invalidate();
+    }
+
+    private void findLine() {
+        int charIndex  = this.editBox.GetCharIndexFromPosition(new Point(0, 0));
+        this.lineIndex = this.editBox.GetLineFromCharIndex(charIndex);
+    }
+
+    private void drawLines(Graphics g) {
+        g.Clear(this.BackColor);
+        int counter = this.lineIndex + 1;
+        int y = 2;
+        int max = 0;
+        while (y < this.ClientRectangle.Height - 15) {
+            SizeF size = g.MeasureString(counter.ToString(), this.Font);
+            g.DrawString(counter.ToString(), this.Font, new SolidBrush(this.ForeColor), new Point(3, y));
+            counter++;
+            y += (int)size.Height;
+            if (max < size.Width) max = (int)size.Width;
+        }
+        max += 6;
+        this.editBox.Location = new Point(max, 0);
+        this.editBox.Size     = new Size(this.ClientRectangle.Width - max, this.ClientRectangle.Height);
+    }
+
+    protected override void OnPaint(PaintEventArgs e) {
+        this.drawLines(e.Graphics);
+        e.Graphics.TranslateTransform(50, 0);
+        this.editBox.Invalidate();
+        base.OnPaint(e);
+    }
+
+    private void onTextChanged(object? sender, EventArgs e) =>
+        this.Text = this.editBox.Text;
+
+    protected override void OnTextChanged(EventArgs e) {
+        if (this.editBox.Text != this.Text)
+            this.editBox.Text = this.Text;
+        base.OnTextChanged(e);
+    }
+
+
+
+
 }

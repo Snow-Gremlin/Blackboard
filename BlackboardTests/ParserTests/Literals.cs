21fabe28e6210cf986910af71f9c3dfcac9a7b77
Grant Nelson <grantnelson1@gmail.com>
2023-09-19 01:12:34 -0600

    Working on literals

---
+++ BlackboardTests/ParserTests/Literals.cs
@@ -0,0 +1,26 @@
+﻿using Blackboard.Core;
+using Blackboard.Core.Extensions;
+using Blackboard.Core.Record;
+using BlackboardTests.Tools;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace BlackboardTests.ParserTests;
+
+[TestClass]
+internal class Literals {
+
+    static private void checkLit<T>(string input, T exp) {
+        Slate slate = new();
+        Result r = slate.Read("a := "+input+";").Perform();
+        T value = (T)r.GetValueAsObject("a");
+        Assert.AreEqual(exp, value, "For: "+input);
+    }
+    
+    [TestMethod]
+    public void TestLiteral_Zero() {
+        checkLit("0", 0);
+        checkLit("00", 0);
+        checkLit("0_0", 0);
+    }
+
+}

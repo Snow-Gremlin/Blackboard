9f0b0564affdae02f6e09a70b846e1646440e61c
Grant Nelson <grantnelson1@gmail.com>
2023-09-19 01:18:41 -0600

    Fixed issue in lang file

--- BlackboardTests/ParserTests/Literals.cs
+++ BlackboardTests/ParserTests/Literals.cs
@@ -7,20 +7,32 @@ using Microsoft.VisualStudio.TestTools.UnitTesting;
 namespace BlackboardTests.ParserTests;
 
 [TestClass]
-internal class Literals {
+public class Literals {
 
     static private void checkLit<T>(string input, T exp) {
         Slate slate = new();
-        Result r = slate.Read("a := "+input+";").Perform();
-        T value = (T)r.GetValueAsObject("a");
+        Result r = slate.Read("get a = "+input+";").Perform();
+        T? value = (T?)r.GetValueAsObject("a");
         Assert.AreEqual(exp, value, "For: "+input);
     }
     
     [TestMethod]
-    public void TestLiteral_Zero() {
+    public void TestLiteral_ZeroInt() {
         checkLit("0", 0);
         checkLit("00", 0);
         checkLit("0_0", 0);
+        checkLit("0b", 0);
+        checkLit("00b", 0);
+        checkLit("0_0b", 0);
+        checkLit("0o", 0);
+        checkLit("00o", 0);
+        checkLit("0_0o", 0);
+        checkLit("0d", 0);
+        checkLit("00d", 0);
+        checkLit("0_0d", 0);
+        checkLit("0x0", 0);
+        checkLit("0x00", 0);
+        checkLit("0x0_0", 0);
     }
 
 }
